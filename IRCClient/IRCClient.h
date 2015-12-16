d63507be0fd7b8baa8ed042e87400cdae009d805
achmizs <achmizs@gmail.com>
2015-12-16 14:47:04 -0500

    Initial Commit

---
+++ IRCClient/IRCClient.h
@@ -0,0 +1,19 @@
+//
+//  IRCClient.h
+//  IRCClient
+//
+//  Created by Sandy Achmiz on 12/16/15.
+//  Copyright © 2015 Said Achmiz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for IRCClient.
+FOUNDATION_EXPORT double IRCClientVersionNumber;
+
+//! Project version string for IRCClient.
+FOUNDATION_EXPORT const unsigned char IRCClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRCClient/PublicHeader.h>
+
+
