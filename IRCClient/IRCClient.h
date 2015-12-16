e4ba2bc3349a01efe82ef25a2cc0311072858063
achmizs <achmizs@gmail.com>
2015-12-16 15:11:02 -0500

    README added

--- IRCClient/IRCClient.h
+++ IRCClient/IRCClient.h
@@ -2,9 +2,19 @@
 //  IRCClient.h
 //  IRCClient
 //
-//  Created by Sandy Achmiz on 12/16/15.
-//  Copyright © 2015 Said Achmiz. All rights reserved.
+//  Copyright © 2015 Said Achmiz.
 //
+/*
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ * License for more details.
+ */
 
 #import <Cocoa/Cocoa.h>
 
